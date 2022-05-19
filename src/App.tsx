e70ec467bfc662b5be8d48090ffa9ff571f23dc3
rkaufusi <rhettkaufusi@gmail.com>
2022-05-19 15:08:28 -0700

    initial layout/ colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <p>Hello world!</p>
+    <div className="flex flex-col items-center bg-gradient-to-r from-yellow-300 to-green-300 h-screen">
+      <h2 className="font-bold text-4xl text-slate-800">JS Method of the day</h2>
+      <div className="flex flex-col text-center outline outline-4 outline-offset-2">
+        <h2>hello</h2>
+        <h4>world</h4>
+        <image></image>
+      </div>
     </div>
   );
 }
