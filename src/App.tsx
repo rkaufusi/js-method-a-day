4bb308f3ab5caec12709a06db79a1273d043d755
rkaufusi <rhettkaufusi@gmail.com>
2022-05-24 14:44:39 -0700

    added styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import methods from './methods'
 
 function App() {
-  console.log(methods[0].image)
+  let date = new Date()
+  let day = date.getDate() - 1
+  let num: number;
+
+  if(day < 20) num = day
+  else num = day - 20
+
   return (
     <div className="flex flex-col items-center bg-gradient-to-r from-yellow-300 to-green-300 h-screen">
-      <h2 className="font-bold text-4xl text-slate-800 m-2">JS Method of the day</h2>
-      <div className="flex flex-col text-center outline outline-4 outline-offset-2">
-        <h1>{methods[0].name}</h1>
-        <h4>{methods[0].description}</h4>
-        <img src={methods[0].image}/>
+      <h2 className="p-3 font-bold text-4xl text-slate-800 m-2">JS Method of the day</h2>
+      <div className="flex flex-col max-w-3xl items-center text-center outline outline-4 outline-offset-2">
+        <h1 className="font-bold text-slate-800 text-3xl p-3">{methods[num].name}</h1>
+        <h4 className="font-semibold text-slate-800 p-2 text-xl">{methods[num].description}</h4>
+        <img className="p-5 max-h-52"src={methods[num].image}/>
       </div>
     </div>
   );
